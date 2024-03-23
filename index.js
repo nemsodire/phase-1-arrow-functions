894895c3dc97cd6eb94627119e43849b8f902bce
neema <neemabridgetteodire2@gmail.com>
2024-03-23 18:02:28 +0300

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const num1=2000
+const num2=100
+function divide(num1,num2) {
+    return (num1/num2)
+}
+let x=2
+const square=(x)=> {
+    return x*x
+}
+let n=4
+let t=3
+const add=(n,t) => {
+    return n+t
+}
